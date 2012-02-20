02c3ef0288d1385e99838a27fb0427c3c50d31b3
davide83 <davide.letizia@gmail.com>
2012-02-20 13:43:08 +0100

    First Commit

---
+++ it_IT.php
@@ -0,0 +1,73 @@
+<?php
+/**********************************************************************************************
+*
+* @File:      mld-newsletter.php
+* @Package:   MLD Newsletter
+* @Subject:   Italian language file
+* @Date:      20 February 2012
+* @Revision:  20 February 2012
+* @Version:   0.4
+* @Status:    Beta
+* @Author:    Davide Letizia (https://github.com/davide83/ItalianLanguage4mld-newsletter)
+*
+***********************************************************************************************/
+
+$i18n['PLUGINNAME'] = "Newsletter";
+$i18n['EMAIL'] = "E-mail";
+$i18n['NAME'] = "Nome";
+$i18n['SUBSCRIBE'] = "Abbonati";
+$i18n['UNSUBSCRIBE'] = "Cancellati";
+$i18n['WRONGEMAIL'] = "Inserisci un indirizzo e-mail valido.";
+$i18n['ALREADYREGISTERED'] = "Indirizzo e-mail già registrato";
+$i18n['CONFIRMEMAILSUBJECT'] = "Conferma l'iscrizione";
+$i18n['CONFIRMEMAILTITLE'] = "Conferma l'iscrizione alla newsletter.";
+$i18n['CONFIRMEMAILBODY1'] = "Ti sei abbonato alla nostra newsletter con i seguenti dati:";
+$i18n['CONFIRMEMAILBODY2'] = "Se le informazioni sono corrette vuoi ricevere la nostra newsletter? Per confermare, fare click sul seguente link:";
+$i18n['CONFIRMATIONSEND'] = "Una e-mail di conferma ti è stata inviata all'indirizzo e-mail fornito";
+$i18n['SUBSCRIBESUCCES'] = "L'indirizzo e-mail è ora abbonato alla nostra newsletter";
+$i18n['SUBSCRIBEFAIL'] = "L'iscrizione dell'indirizzo e-mail è fallita. Prova ancora o Contattaci.";
+$i18n['CONFIRMAGAIN'] = "Questo indirizzo e-mail è già registrato, ma non confermato. Una nuova e-mail di conferma ti sarà invia";
+$i18n['MANAGESUBSCRIBERS'] = "Gestisci abbonati";
+$i18n['MANAGELETTERS'] = "Gestisci newsletters";
+$i18n['EDITLETTER'] = "Modifica newsletter";
+$i18n['SETTINGS'] = "Impostazioni newsletter";
+$i18n['SMTPSETTINGS'] = "Impostazioni SMTP";
+$i18n['CONFIRMATIONNOTFOUND'] = "Codice di Conferma non trovato.";
+$i18n['EMAILNOTFOUND'] = "E-mail non trovata";
+$i18n['UNSUBSCRIBESUCCES'] = "Questa e-mail ora è abbonata";
+$i18n['LETTERTITLE'] = "Titolo Newsletter";
+$i18n['ALREADYSEND'] = "Già spedita";
+$i18n['NEWLETTER'] = "Crea nuova newsletter";
+$i18n['LETTERSAVED'] = "Newsletter salvata";
+$i18n['LETTERSAVEFAIL'] = "Impossibile salvare la Newsletter";
+$i18n['SENDFAIL'] = "L'invio della newsletter agli abbonati è fallito";
+$i18n['SENDSUCCESS'] = "Newsletter inviata agli abbonati";
+$i18n['TESTSEND'] = "Test invia all'amministratore";
+$i18n['SEND'] = "Invia newsletter agli abbonati";
+$i18n['VIEWLETTER'] = "Visualizza & Invia newsletter";
+$i18n['SENDWARNING'] = "Stai per inviare la newsletter a tutti gli abbonati. Ne sei sicuro?";
+$i18n['SENDFAILADMIN'] = "Fallito l'invio della newsletter a ";
+$i18n['SENDSUCCESSADMIN'] = "Newsletter inviata a ";
+$i18n['SENDERNAME'] = "Nome mittente";
+$i18n['SENDEREMAIL'] = "E-mail mittente";
+$i18n['SAVE_SETTINGS'] = "Salva impostazioni";
+$i18n['SETTINGSFILE_NOTFOUND'] = "File delle Impostazioni non trovato";
+$i18n['SAVESUCCESS'] = "Cambiamenti salvati con successo";
+$i18n['SAVEFAIL'] = "Impossibile salvare i cambiamenti";
+$i18n['SAVELETTER'] = "Salva newsletter";
+$i18n['TEMPLATESETTINGS'] = "Impostazioni Modello";
+$i18n['TEMPLATEHEADER'] = "Intestazione Modello";
+$i18n['TEMPLATEFOOTER'] = "Piè di Pagina Modello";
+$i18n['STATUS'] = "Stato";
+$i18n['SMTPSERVER'] = "SMTP mailserver";
+$i18n['SMTPPORT'] = "SMTP numero porta";
+$i18n['SMTPAUTH'] = "Abilita l'accesso SMTP <b style=\"font-weight:100\"> - usa queste impostazioni:</b>";
+$i18n['SMTPSECURE'] = "SMTP sicuro (SSL or TLS)";
+$i18n['SMTPAUTHNAME'] = "SMTP nome di accesso";
+$i18n['SMTPAUTHPW'] = "SMTP password";
+$i18n['DELETESUCCES'] = "Newsletter eliminata con successo.";
+$i18n['DELETEFAIL'] = "L'eliminazione della newsletter è fallita.";
+
+//since 0.4
+$i18n['CREATEDNEWFILE'] = "Datafile non trovato. Un nuovo datafile viene creato per:";
+$i18n['COULDNOTCREATEFILE'] = "Datafile non trovato. Impossibile creare un nuovo datafile per:";
